c4e8b5ce591fcd773de1418e538aa02062cbba97
mickey <wl920425@gmail.com>
2017-01-02 23:28:49 +0900

    use HttpClient in crawler

--- crawler.py
+++ crawler.py
@@ -1,62 +1,49 @@
 from asyncio import Queue
 from configparser import ConfigParser
-from form import FormSubmitter
-from bs4 import BeautifulSoup
-import aiohttp
 import asyncio
+from http_client import HttpClient
 from link_parser import LinkParser
-import time
-from traffic_controller import TrafficController
 
 
 class Crawler:
-    def __init__(self, root_url, max_redirect, loop, max_tasks, encoding, max_requests_in_one_second=2):
-        self.maxTasks = max_tasks
+    def __init__(self, root_url, max_redirect, max_tasks, encoding, http_client: HttpClient):
+        self.max_tasks = max_tasks
         self.max_redirect = max_redirect
         self.encoding = encoding
         self.q = Queue()
         self.q.put_nowait((root_url, self.max_redirect))
         self.seen_urls = set()
 
-        self.session = aiohttp.ClientSession(loop = loop)
-        self.formSubmitter = FormSubmitter(self.session)
+        self.http_client = http_client
 
         self.config = ConfigParser()
         self.config.read('auth.ini')
-
-        self.traffic_controller = TrafficController(max_requests_in_one_second)
 
     @asyncio.coroutine
     def login(self):
         login_url="http://www.sexinsex.net/bbs/index.php"
         form_selector="#loginform"
         parameters = {"username": self.config["login"]["username"], "password": self.config["login"]["password"]}
-
-        login_response = yield from self.formSubmitter.submit(
+        soup = yield from self.http_client.submit(
             login_url=login_url, form_selector=form_selector, parameters=parameters, encoding=self.encoding)
-        try:
-            html = yield from login_response.text(self.encoding)
-            soup = BeautifulSoup(html, "html.parser")
-            message = soup.select_one(".box.message").get_text()
-            if message.find(u'欢迎您回来') != -1:
-                print("login successfully: " + message)
-                return True
 
-            print("login failed, returned message is " + message)
-            return False
-        finally:
-            yield from login_response.release()
+        message = soup.select_one(".box.message").get_text()
+        if message.find(u'欢迎您回来') != -1:
+            print("login successfully: " + message)
+            return True
+        print("login failed, returned message is " + message)
+        return False
 
     @asyncio.coroutine
     def crawl(self):
         login_successful = yield from self.login()
         if not login_successful:
             return False
-        workers = [asyncio.Task(self.work()) for _ in range(self.maxTasks)]
+        workers = [asyncio.Task(self.work()) for _ in range(self.max_tasks)]
         yield from self.q.join()
         for w in workers:
             w.cancel()
-        self.session.close()
+        self.http_client.close()
 
     @asyncio.coroutine
     def work(self):
@@ -67,25 +54,20 @@ class Crawler:
 
     @asyncio.coroutine
     def fetch(self, url, max_redirect):
-        yield from self.traffic_controller.wait_before_request()
-
-        print("request for page(max_redirect={}): {}".format(max_redirect, url))
-        response = yield from self.session.get(url, allow_redirects=False)
-
+        response = yield from self.http_client.get_raw_response(url)
         try:
             if response.status == 302:
                 if max_redirect > 0:
-                    nextUrl = response.headers['location']
-                    if nextUrl in self.seen_urls:
+                    next_url = response.headers['location']
+                    if next_url in self.seen_urls:
                         return
-                    self.seen_urls.add(nextUrl)
-                    self.q.put_nowait((nextUrl, max_redirect - 1))
+                    self.seen_urls.add(next_url)
+                    self.q.put_nowait((next_url, max_redirect - 1))
             else:
                 html = yield from response.text(self.encoding)
-                print("fetched page(max_redirect={}): {}".format(max_redirect, url))
                 links = LinkParser(html, url).parse_links()
                 for link in links.difference(self.seen_urls):
                     self.q.put_nowait((link, self.max_redirect))
                 self.seen_urls.update(links)
         finally:
-            yield from response.release()
+            response.release()
--- test.py
+++ test.py
@@ -1,8 +1,15 @@
 import asyncio
+
+import aiohttp
+
 from crawler import Crawler
+from http_client import HttpClient
+from traffic_controller import TrafficController
 
 loop = asyncio.get_event_loop()
 
-crawler = Crawler("http://www.sexinsex.net/bbs/forum-186-1.html", max_redirect=10, max_tasks=10, loop=loop, encoding="GBK", max_requests_in_one_second=2.5)
+http_client = HttpClient(aiohttp.ClientSession(loop = loop), TrafficController(2.5))
+
+crawler = Crawler("http://www.sexinsex.net/bbs/forum-186-1.html", max_redirect=10, max_tasks=10, http_client=http_client, encoding="GBK")
 
 loop.run_until_complete(crawler.crawl())
